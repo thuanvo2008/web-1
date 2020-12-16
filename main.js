8d67908b39425e9e5bb54ed9dd12ddb1bc52bfbc
kieetienn <iamtien277@gmail.com>
2020-12-16 13:42:43 +0700

    update boy and girl

--- main.js
+++ main.js
@@ -3,22 +3,21 @@
 var modal = document.getElementById("myModal");
 var btn = document.getElementById("cart");
 var close = document.getElementsByClassName("close")[0];
-// tại sao lại có [0] như  thế này bởi vì mỗi close là một html colection nên khi mình muốn lấy giá trị html thì phải thêm [0]. 
-//Nếu mình có 2 cái component cùng class thì khi [0] nó sẽ hiển thị component 1 còn [1] thì nó sẽ hiển thị component 2.
+//có [0]vì mỗi close là một html colection nên khi mình muốn lấy giá trị html thì phải thêm [0]. 
+//Nếu có 2 cái component cùng class thì khi [0] nó sẽ hiển thị component 1 còn [1] thì nó sẽ hiển thị component 2.
 var close_footer = document.getElementsByClassName("close-footer")[0];
 var order = document.getElementsByClassName("order")[0];
 btn.onclick = function () {
-  modal.style.display = "block";
+  modal.style.display = "block";    //click icon giỏ hàng hiện ra
 }
-close.onclick = function () {
+close.onclick = function () {       //click x giỏ hàng biến mất
   modal.style.display = "none";
 }
-close_footer.onclick = function () {
+close_footer.onclick = function () {    //click button close biến mất
   modal.style.display = "none";
 }
 order.onclick = function () {
-  // alert("Cảm ơn bạn đã thanh toán đơn hàng")
-  location.assign("login.html")
+  location.assign("payment.html")
 }
 window.onclick = function (event) {
   if (event.target == modal) {
@@ -27,13 +26,11 @@ window.onclick = function (event) {
 }
 
 
-
-
 // xóa cart
 var remove_cart = document.getElementsByClassName("btn-danger");
 for (var i = 0; i < remove_cart.length; i++) {
   var button = remove_cart[i]
-  button.addEventListener("click", function () {
+  button.addEventListener("click", function () {    //nếu button bắt sự kiện click thì function sẽ được thực thi
     var button_remove = event.target
     button_remove.parentElement.parentElement.remove();
     updatecart();
@@ -47,15 +44,15 @@ function updatecart() {
     var cart_rows = cart_item.getElementsByClassName("cart-row");
     var total = 0;
     for (var i = 0; i < cart_rows.length; i++) {
-      var cart_row = cart_rows[i]
-      var price_item = cart_row.getElementsByClassName("cart-price ")[0]
-      var quantity_item = cart_row.getElementsByClassName("cart-quantity-input")[0]
+      var cart_row = cart_rows[i];
+      var price_item = cart_row.getElementsByClassName("cart-price ")[0];
+      var quantity_item = cart_row.getElementsByClassName("cart-quantity-input")[0];
       var price = parseFloat(price_item.innerText)// chuyển một chuổi string sang number để tính tổng tiền.
       var quantity = quantity_item.value // lấy giá trị trong thẻ input
-      total = total + (price * quantity)
+      total = total + (price * quantity);
     }
-    document.getElementsByClassName("cart-total-price")[0].innerText = total + ' Dola'
-    // Thay đổi text = total trong .cart-total-price. Chỉ có một .cart-total-price nên mình sử dụng [0].
+    document.getElementsByClassName("cart-total-price")[0].innerText = total + '$';
+    // Thay đổi text = total trong .cart-total-price. Chỉ có một .cart-total-price nên sử dụng [0].
   }
 
 
@@ -65,7 +62,7 @@ var quantity_input = document.getElementsByClassName("cart-quantity-input");
 for (var i = 0; i < quantity_input.length; i++) {
   var input = quantity_input[i];
   input.addEventListener("change", function (event) {
-    var input = event.target
+    var input = event.target;
     if (isNaN(input.value) || input.value <= 0) {
       input.value = 1;
     }
@@ -83,11 +80,12 @@ for (var i = 0; i < add_cart.length; i++) {
     var button = event.target;
     var product = button.parentElement.parentElement;
     var img = product.parentElement.getElementsByClassName("img-prd")[0].src;
-    var title = product.getElementsByClassName("content-product-h3")[0].innerText
-    var price = product.getElementsByClassName("price")[0].innerText
-    addItemToCart(title, price, img)
+    var title = product.getElementsByClassName("content-product-h3")[0].innerText;
+    var price = product.getElementsByClassName("price")[0].innerText;
+    addItemToCart(title, price, img);
+    alert("Added product to cart");
     // Khi thêm sản phẩm vào giỏ hàng thì sẽ hiển thị modal
-    modal.style.display = "block";
+    // modal.style.display = "block";
     
     updatecart()
   })
@@ -98,10 +96,10 @@ function addItemToCart(title, price, img) {
   cartRow.classList.add('cart-row')
   var cartItems = document.getElementsByClassName('cart-items')[0]
   var cart_title = cartItems.getElementsByClassName('cart-item-title')
-  //Nếu title của sản phẩm bằng với title mà bạn thêm vao giỏ hàng thì sẽ thông cho user.
+  //Nếu title của sản phẩm bằng với title đã thêm vào giỏ hàng thì sẽ thông báo cho user.
   for (var i = 0; i < cart_title.length; i++) {
     if (cart_title[i].innerText == title) {
-      alert('Sản Phẩm Đã Có Trong Giỏ Hàng')
+      alert("Products already in the cart")
       return;
    }
   }
